417084a83bbe42d6b60d863cdd98665896e01fe6
Ayush Shukla <iCodeAyush@gmail.com>
2021-04-20 03:47:50 +0530

    x

--- JS/App.js
+++ JS/App.js
@@ -7,6 +7,17 @@ function LandingPageScrollTrigger() {
         opacity: 1, duration: 1.3,
     }) // /LoadingAnimation---------------------
 
+    $("#codeby a").mouseenter(function () { // HoverAnimation---------------------
+        gsap.to('#ImgWrapper', { backgroundColor: "#f0f0f0", })
+        gsap.to('#codeby a', { color: "#2e2e2e", })
+        gsap.to('#codeby a span', { color: "#000000", })
+    });
+    $("#codeby a").mouseout(function () {
+        gsap.to('#ImgWrapper', { backgroundColor: "#000000", })
+        gsap.to('#codeby a', { color: "#e6e6e6", })
+        gsap.to('#codeby a span', { color: "#f0f0f0", })
+    }); // /HoverAnimation---------------------
+
     let LandingPageScrollTrigger = gsap.timeline({
         scrollTrigger: {
             trigger: "#ImgWrapper",
@@ -24,6 +35,8 @@ function LandingPageScrollTrigger() {
         .to('#ImgWrapper #img3', { transform: 'translateZ(2600px)', }, 0)
         .to('#ImgWrapper #img2', { transform: 'translateZ(2400px)', }, 0)
         .to('#ImgWrapper #img1', { transform: 'translateZ(2200px)', }, 0)
+        .set('#codeby a', { opacity: 0 }, 0)
+        .from('#codeby a', { y: 130, opacity: 0 }, 0.31)
 }
 //---------------------/Landing Page ScrollTrigger---------------------
 
